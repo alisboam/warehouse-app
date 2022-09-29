3a0b0299fff5778bdf7eb54bfa8ec30721cbf231
Aline Moraes <alisboam@gmail.com>
2022-09-29 14:57:50 -0300

    exibe o nome de usuário logado

--- spec/system/warehouse/user_sign_up_spec.rb
+++ spec/system/warehouse/user_sign_up_spec.rb
@@ -8,6 +8,7 @@ describe 'Usuário se autentica' do
       click_on 'Entrar'
     end
     click_on 'Criar conta'
+    fill_in 'Nome', with: 'João'
     fill_in 'E-mail', with: 'joao@email.com'
     fill_in 'Senha', with: 'password'
     fill_in 'Confirme sua senha', with: 'password'
@@ -15,9 +16,11 @@ describe 'Usuário se autentica' do
 
     within('nav') do
       expect(page).to have_content 'joao@email.com'
+      expect(page).to have_content 'João'
       expect(page).not_to have_link 'Entrar'
       expect(page).to have_button 'Sair'
     end
+    expect(User.last.name).to eq 'João'
     expect(page).to have_content 'Bem vindo! Você realizou seu registro com sucesso.'
   end
 end
